0dc3839c7fd9e0036d14c072653c8c049c479c5b
Pedro Medina <pmedina@kuantum.cl>
2020-06-11 13:59:29 -0400

    first

---
+++ src/Space.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let space = '';
+</script>
+
+<style>
+    .space {
+        border: solid 1px gray;
+        height: 125px;
+        width: 125px;
+        flex: 1;
+        align-items: center;
+        justify-content: center;
+        font-size: 75px;
+        line-height: 125px;
+        text-align: center;
+    }
+</style>
+
+<div class="space">
+    {space}
+</div>
